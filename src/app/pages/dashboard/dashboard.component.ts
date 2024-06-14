21a1c86c7a86cff0d4d94d80f351b102b2c0622e
TuanLily <122657799+TuanLily@users.noreply.github.com>
2024-06-14 13:38:56 +0700

    [DOT-22] - Chỉnh sửa giao diện trang dasboard
    [DOT-22] - Chỉnh sửa giao diện trang dasboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { formatCurrency } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { NbThemeService } from '@nebular/theme';
 import { IProductPrices } from 'app/@core/interfaces/statistic.interface';
 import { statisticsService } from 'app/@core/services/apis/statistic.service';
 @Component({
@@ -14,45 +15,116 @@ export class DashboardComponent implements OnInit {
 
   productPriceStats: IProductPrices;
 
+  themes = [
+    { value: 'default', name: 'Light' },
+    { value: 'dark', name: 'Dark' },
+  ];
+
+  currentTheme = 'default';
+
 
   constructor(
-    private statisticsService: statisticsService
+    private statisticsService: statisticsService,
+    private themeService: NbThemeService,
+
 
   ) {
+    this.themeService.onThemeChange()
+      .subscribe(theme => {
+        this.currentTheme = theme.name;
+      });
+
     this.columnChartOptions = {
-      xAxis: {
-        type: 'category',
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      title: {
+        text: 'Biểu đồ'
       },
-      yAxis: {
-        type: 'value',
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'shadow'
+        }
       },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      xAxis: [
+        {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+          axisTick: {
+            alignWithLabel: true
+          }
+        }
+      ],
+      yAxis: [
+        {
+          type: 'value'
+        }
+      ],
       series: [
         {
-          name: 'Biểu đồ cột 1',
+          name: 'Direct',
           type: 'bar',
-          data: [150, 230, 224, 218, 135, 147, 260],
-        },
-      ],
+          barWidth: '60%',
+          data: [10, 52, 200, 334, 390, 330, 220]
+        }
+      ]
     };
 
     this.pieChartOptions = {
+      title: {
+        text: 'Biểu đồ'
+      },
+      tooltip: {
+        trigger: 'item'
+      },
+      legend: {
+        top: '5%',
+        left: 'center'
+      },
       series: [
         {
-          name: 'Biểu đồ tròn',
+          name: 'Access From',
           type: 'pie',
-          radius: '50%',
+          radius: ['40%', '70%'],
+          avoidLabelOverlap: false,
+          itemStyle: {
+            borderRadius: 10,
+            borderColor: '#fff',
+            borderWidth: 2
+          },
+          label: {
+            show: false,
+            position: 'center'
+          },
+          emphasis: {
+            label: {
+              show: true,
+              fontSize: 40,
+              fontWeight: 'bold'
+            }
+          },
+          labelLine: {
+            show: false
+          },
           data: [
-            { value: 335, name: 'Mùa Xuân' },
-            { value: 310, name: 'Mùa Hè' },
-            { value: 234, name: 'Mùa Thu' },
-            { value: 135, name: 'Mùa Đông' },
-          ],
-        },
-      ],
+            { value: 1048, name: 'Search Engine' },
+            { value: 735, name: 'Direct' },
+            { value: 580, name: 'Email' },
+            { value: 484, name: 'Union Ads' },
+            { value: 300, name: 'Video Ads' }
+          ]
+        }
+      ]
     };
 
     this.lineChartOptions = {
+      title: {
+        text: 'Biểu đồ'
+      },
       xAxis: {
         type: 'category',
         data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
