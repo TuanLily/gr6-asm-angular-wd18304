2b7a8d73fbcb7f1b70096a7b062b705d3177c2c0
haohtpc05526 <trunghaopc23579@gmail.com>
2024-06-18 12:10:27 +0700

    [DOT-20.4] - Chỉnh sửa validators vouchers
    [DOT-20.4] - Chỉnh sửa validators vouchers, bắt trùng lặp và ngày có hiệu lực không được lớn hơn hết hiệu lực

--- src/app/@core/services/apis/voucher.service.ts
+++ src/app/@core/services/apis/voucher.service.ts
@@ -6,48 +6,53 @@ import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
 import { ApiService } from '../common';
-import { API_BASE_URL, API_ENDPOINT } from "../../config/api-endpoint.config";
+import { API_BASE_URL, API_ENDPOINT } from '../../config/api-endpoint.config';
 import { IVoucher } from 'app/@core/interfaces/vouchers.interface';
 
-
-
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class VoucherService extends ApiService {
+  constructor(private _http: HttpClient) {
+    super(_http);
+  }
 
-    constructor(private _http: HttpClient) {
-        super(_http);
-    }
+  getAllVouchers(page: number, search: string = ''): Observable<any> {
+    const params = { page: page.toString(), search: search };
+    return this.get<any>(API_BASE_URL + API_ENDPOINT.vouchers, params);
+  }
 
-    getAllVouchers(page: number, search: string = ''): Observable<any> {
-        const params = { page: page.toString(), search: search };
-        return this.get<any>(API_BASE_URL + API_ENDPOINT.vouchers, params);
-    }
+  addVoucher(voucher: IVoucher): Observable<any> {
+    return this.post<any>(API_BASE_URL + API_ENDPOINT.vouchers, voucher);
+  }
 
-    addVoucher(voucher: IVoucher): Observable<any> {
-        return this.post<any>(API_BASE_URL + API_ENDPOINT.vouchers, voucher);
-    }
+  updateVoucher(voucherId: number, voucher: IVoucher): Observable<any> {
+    const url = `${API_BASE_URL + API_ENDPOINT.vouchers}/${voucherId}`;
+    return this.patch<any>(url, voucher);
+  }
 
-    updateVoucher(voucherId: number, voucher: IVoucher): Observable<any> {
-        const url = `${API_BASE_URL + API_ENDPOINT.vouchers}/${voucherId}`;
-        return this.patch<any>(url, voucher);
-    }
+  deleteVoucher(voucherId: number): Observable<any> {
+    const url = `${API_BASE_URL + API_ENDPOINT.vouchers}/${voucherId}`;
+    return this.delete<void>(url);
+  }
 
-    deleteVoucher(voucherId: number): Observable<any> {
-        const url = `${API_BASE_URL + API_ENDPOINT.vouchers}/${voucherId}`;
-        return this.delete<void>(url);
-    }
+  getVoucherByCode(code: string): Observable<any> {
+    const params = new HttpParams().set('code', code);
+    const url = `${API_BASE_URL}${API_ENDPOINT.vouchers}/code`;
+    return this._http.get<any>(url, { params }).pipe(
+      catchError((error) => {
+        // console.error('Error fetching voucher by code:', error);
+        return throwError(error);
+      })
+    );
+  }
 
-    getVoucherByCode(code: string): Observable<any> {
-      const params = new HttpParams().set('code', code);
-      const url = `${API_BASE_URL}${API_ENDPOINT.vouchers}/code`;
-      return this._http.get<any>(url, { params })
-          .pipe(
-              catchError(error => {
-                  // console.error('Error fetching voucher by code:', error);
-                  return throwError(error);
-              })
-          );
+  checkDuplicateVoucherCode(voucherCode: string): Observable<any> {
+    const url = `${API_BASE_URL}${API_ENDPOINT.vouchers}/check-duplicate/${voucherCode}`;
+    return this._http.get<any>(url).pipe(
+      catchError((error) => {
+        return throwError(error);
+      })
+    );
   }
 }
--- src/app/pages/vouchers/list/list.component.ts
+++ src/app/pages/vouchers/list/list.component.ts
@@ -1,48 +1,37 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ValidatorFn } from '@angular/forms';
-import { NbThemeService, NbToastrService } from '@nebular/theme';
+import { FormGroup, FormControl, Validators, ValidatorFn, ValidationErrors  } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-
+import { NbThemeService, NbToastrService } from '@nebular/theme';
 import Swal from 'sweetalert2';
-
 import { IVoucher } from 'app/@core/interfaces/vouchers.interface';
 import { VoucherService } from 'app/@core/services/apis/voucher.service';
 import { SpinnerService } from '../../../@theme/components/spinner/spinner.service';
-import { API_BASE_URL, API_ENDPOINT } from 'app/@core/config/api-endpoint.config';
-
-const dateComparisonValidator: ValidatorFn = (control: FormGroup): { [key: string]: boolean } | null => {
-  const validFrom = control.get('valid_from').value;
-  const validTo = control.get('valid_to').value;
-
-  if (validFrom && validTo) {
-      const fromDate = new Date(validFrom);
-      const toDate = new Date(validTo);
-      const currentDate = new Date();
-
-      if (toDate <= fromDate || toDate <= currentDate) {
-          return { 'dateInvalid': true };
-      }
-  }
-
-  return null;
-};
+import {
+  API_BASE_URL,
+  API_ENDPOINT,
+} from 'app/@core/config/api-endpoint.config';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
   @ViewChild('formElement') formElement: ElementRef;
 
   form: FormGroup;
-  newVoucher: IVoucher = { voucher_code: '', discount_rate: 0, valid_from: '', valid_to: '', description: '' };
+  newVoucher: IVoucher = {
+    voucher_code: '',
+    discount_rate: 0,
+    valid_from: '',
+    valid_to: '',
+    description: '',
+  };
 
   isAddingNewVoucher: boolean = true;
   isEditing: boolean = false;
 
   vouchers: IVoucher[] = [];
-
   apiUrl = API_BASE_URL + API_ENDPOINT.vouchers;
   currentPage: number = 1;
   totalPages: number;
@@ -61,28 +50,38 @@ export class ListComponent implements OnInit {
     private themeService: NbThemeService,
     private spinner: SpinnerService,
     private router: Router,
-    private route: ActivatedRoute,
-    private formBuilder: FormBuilder
+    private route: ActivatedRoute
   ) {
-    this.themeService.onThemeChange()
-      .subscribe(theme => {
-        this.currentTheme = theme.name;
-      });
+    this.themeService.onThemeChange().subscribe((theme) => {
+      this.currentTheme = theme.name;
+    });
   }
 
   ngOnInit(): void {
-    this.route.queryParams.subscribe(params => {
+    this.route.queryParams.subscribe((params) => {
       const currentPage = params['page'] || 1;
       this.loadVouchers(currentPage);
     });
 
-    this.form = this.formBuilder.group({
-      voucher_code: ['', Validators.required],
-      discount_rate: ['', [Validators.required, Validators.min(0), Validators.max(100)]],
-      valid_from: ['', Validators.required],
-      valid_to: ['', Validators.required],
-      description: ['', Validators.required]
-    }, { validator: dateComparisonValidator });
+    this.form = new FormGroup({
+      voucher_code: new FormControl('', Validators.required),
+      discount_rate: new FormControl('', [
+        Validators.required,
+        Validators.min(0),
+        Validators.max(100),
+      ]),
+      valid_from: new FormControl('', [Validators.required, this.dateRangeValidator]),
+      valid_to: new FormControl('', [
+        Validators.required,
+        this.validateEndDate.bind(this),
+      ]),
+      description: new FormControl(''),
+    });
+  }
+
+  minValidDate(): string {
+    const today = new Date();
+    return today.toISOString().split('T')[0];
   }
 
   formatDate(value: string): string {
@@ -93,26 +92,39 @@ export class ListComponent implements OnInit {
     return `${year}-${month}-${day}`;
   }
 
+  dateRangeValidator(control: FormControl): ValidationErrors | null {
+    const formGroup = control.parent;
+    if (!formGroup) return null;
+
+    const validFrom = formGroup.get('valid_from').value;
+    const validTo = formGroup.get('valid_to').value;
+
+    if (validFrom && validTo && new Date(validFrom) > new Date(validTo)) {
+      return { dateRangeInvalid: true };
+    }
+    return null;
+  }
+  
   loadVouchers(page: number): void {
     this.spinner.show();
+    this.voucherService
+      .getAllVouchers(page, this.searchQuery)
+      .subscribe((data) => {
+        this.spinner.hide();
+        this.vouchers = data.vouchers;
+        this.currentPage = data.currentPage;
+        this.totalPages = data.totalPages;
 
-    this.voucherService.getAllVouchers(page, this.searchQuery).subscribe(data => {
-      this.spinner.hide();
-      this.vouchers = data.vouchers;
-      this.currentPage = data.currentPage;
-      this.totalPages = data.totalPages;
+        const queryParams: any = { page: page };
+        if (this.searchQuery && this.searchQuery.trim() !== '') {
+          queryParams.search = this.searchQuery;
+        }
 
-      const queryParams: any = { page: page };
-
-      if (this.searchQuery && this.searchQuery.trim() !== '') {
-        queryParams.search = this.searchQuery;
-      }
-
-      this.router.navigate([], {
-        queryParams: queryParams,
-        replaceUrl: true
+        this.router.navigate([], {
+          queryParams: queryParams,
+          replaceUrl: true,
+        });
       });
-    });
   }
 
   onSearch(): void {
@@ -121,51 +133,82 @@ export class ListComponent implements OnInit {
 
   addVoucher(): void {
     if (!this.form.valid) {
-      this.toastrService.danger('Vui lòng nhập đủ dữ liệu và kiểm tra lại các trường!', 'Error');
+      this.toastrService.danger(
+        'Vui lòng nhập đủ dữ liệu và kiểm tra lại các trường!',
+        'Error'
+      );
       return;
     }
 
-    let voucherData: IVoucher = {
-      voucher_code: this.form.get('voucher_code').value,
-      discount_rate: this.form.get('discount_rate').value,
-      valid_from: this.form.get('valid_from').value,
-      valid_to: this.form.get('valid_to').value,
-      description: this.form.get('description').value
-    };
+    const voucherCode = this.form.get('voucher_code').value;
 
-    this.spinner.show();
-
-    if (this.isEditing && !this.isAddingNewVoucher) {
-      const voucherId = this.newVoucher.id;
-      this.voucherService.updateVoucher(voucherId, voucherData).subscribe(
-        () => {
-          this.toastrService.success('Cập nhật thành công!', 'Success');
-          this.isEditing = false;
-          this.spinner.hide();
-          this.loadVouchers(this.currentPage);
-        },
-        error => {
-          this.toastrService.danger('Đã xảy ra lỗi khi cập nhật voucher!', 'Error');
-          console.error('Error updating voucher:', error);
-          this.spinner.hide();
+    this.voucherService.checkDuplicateVoucherCode(voucherCode).subscribe(
+      (response) => {
+        if (
+          response.exists &&
+          (this.isAddingNewVoucher ||
+            (this.isEditing && voucherCode !== this.newVoucher.voucher_code))
+        ) {
+          this.form.get('voucher_code').setErrors({ duplicate: true });
+          return;
         }
-      );
-    } else {
-      this.voucherService.addVoucher(voucherData).subscribe(
-        () => {
-          this.toastrService.success('Thêm mới thành công!', 'Success');
-          this.spinner.hide();
-          this.loadVouchers(1);
-        },
-        error => {
-          this.toastrService.danger('Đã xảy ra lỗi khi thêm voucher!', 'Error');
-          console.error('Error adding voucher:', error);
-          this.spinner.hide();
-        }
-      );
-    }
 
-    this.form.reset();
+        let voucherData: IVoucher = {
+          voucher_code: voucherCode,
+          discount_rate: this.form.get('discount_rate').value,
+          valid_from: this.form.get('valid_from').value,
+          valid_to: this.form.get('valid_to').value,
+          description: this.form.get('description').value,
+        };
+
+        this.spinner.show();
+
+        if (this.isEditing && !this.isAddingNewVoucher) {
+          const voucherId = this.newVoucher.id;
+          this.voucherService.updateVoucher(voucherId, voucherData).subscribe(
+            () => {
+              this.toastrService.success('Cập nhật thành công!', 'Success');
+              this.isEditing = false;
+              this.spinner.hide();
+              this.loadVouchers(this.currentPage);
+            },
+            (error) => {
+              this.toastrService.danger(
+                'Đã xảy ra lỗi khi cập nhật voucher!',
+                'Error'
+              );
+              console.error('Error updating voucher:', error);
+              this.spinner.hide();
+            }
+          );
+        } else {
+          this.voucherService.addVoucher(voucherData).subscribe(
+            () => {
+              this.toastrService.success('Thêm mới thành công!', 'Success');
+              this.spinner.hide();
+              this.loadVouchers(1);
+            },
+            (error) => {
+              this.toastrService.danger(
+                'Đã xảy ra lỗi khi thêm voucher!',
+                'Error'
+              );
+              console.error('Error adding voucher:', error);
+              this.spinner.hide();
+            }
+          );
+        }
+
+        this.form.reset();
+      },
+      (error) => {
+        this.toastrService.danger(
+          'Đã xảy ra lỗi khi kiểm tra mã voucher!',
+          'Error'
+        );
+        console.error('Error checking duplicate voucher:', error);
+      }
+    );
   }
 
   editVoucher(voucherId: number): void {
@@ -173,7 +216,9 @@ export class ListComponent implements OnInit {
       return;
     }
 
-    const voucherIndex = this.vouchers.findIndex(voucher => voucher.id === voucherId);
+    const voucherIndex = this.vouchers.findIndex(
+      (voucher) => voucher.id === voucherId
+    );
     if (voucherIndex !== -1) {
       this.newVoucher = { ...this.vouchers[voucherIndex] };
 
@@ -182,7 +227,7 @@ export class ListComponent implements OnInit {
         discount_rate: this.newVoucher.discount_rate,
         valid_from: this.formatDate(this.newVoucher.valid_from),
         valid_to: this.formatDate(this.newVoucher.valid_to),
-        description: this.newVoucher.description
+        description: this.newVoucher.description,
       });
       this.isAddingNewVoucher = false;
       this.isEditing = true;
@@ -198,20 +243,25 @@ export class ListComponent implements OnInit {
       icon: 'warning',
       showCancelButton: true,
       confirmButtonText: 'Đồng ý',
-      cancelButtonText: 'Hủy bỏ'
+      cancelButtonText: 'Hủy bỏ',
     }).then((result) => {
       if (result.isConfirmed) {
         this.voucherService.deleteVoucher(voucherId).subscribe(
           () => {
-            const voucherIndex = this.vouchers.findIndex(voucher => voucher.id === voucherId);
+            const voucherIndex = this.vouchers.findIndex(
+              (voucher) => voucher.id === voucherId
+            );
             if (voucherIndex !== -1) {
               this.vouchers.splice(voucherIndex, 1);
               this.loadVouchers(this.currentPage);
               this.toastrService.success('Xóa thành công!', 'Success');
             }
           },
-          error => {
-            this.toastrService.danger('Đã xảy ra lỗi khi xóa voucher!', 'Error');
+          (error) => {
+            this.toastrService.danger(
+              'Đã xảy ra lỗi khi xóa voucher!',
+              'Error'
+            );
             console.error('Error deleting voucher:', error);
           }
         );
@@ -223,16 +273,33 @@ export class ListComponent implements OnInit {
     this.isEditing = false;
     this.isAddingNewVoucher = false;
     this.form.reset();
-    this.toastrService.success('Dữ liệu trên form đã được reset!', 'Thành công');
+    this.toastrService.success(
+      'Dữ liệu trên form đã được reset!',
+      'Thành công'
+    );
   }
 
   scrollFormIntoView() {
     if (this.formElement) {
-      this.formElement.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      this.formElement.nativeElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
     }
   }
 
   formatCurrency(value: number): string {
-    return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + ' đ';
+    return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') + ' đ';
+  }
+
+  validateEndDate(control: FormControl): { [s: string]: boolean } {
+    if (control.value) {
+      const today = new Date();
+      const endDate = new Date(control.value);
+      if (endDate < today) {
+        return { invalidDate: true };
+      }
+    }
+    return null;
   }
 }
